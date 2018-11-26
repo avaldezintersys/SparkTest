65c1645f69f4d388134574a2cdc0626963af5669
Aldo Valdez <avaldez@intersysconsulting.com>
2018-11-26 15:23:41 -0600

    Added several fixes

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # SparkTest
 
-SparkTest
-----
-First example for spark & java develop
+## Start Spark Shell
+spark-shell --master spark://spark:7077
+
+## View Spark Master WebUI console
+http://192.168.99.100:8080/
+
+## View Spark Worker WebUI console
+http://192.168.99.100:8081/
+
+## Run
+
+- spark-submit --class com.test.spark.SparkFileLineCounter --master spark://spark:7077 /data/sparkTest-0.0.1-SNAPSHOT.jar
+
+- spark-submit --class com.test.spark.SparkListCounter --master spark://spark:7077 /data/sparkTest-0.0.1-SNAPSHOT.jar
+
--- sparkTest/src/main/java/com/test/spark/SparkListCounter.java
+++ sparkTest/src/main/java/com/test/spark/SparkListCounter.java
@@ -7,11 +7,11 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
-public class SparkLineCounter {
+public class SparkListCounter {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		SparkConf conf = new SparkConf().setAppName("LineCounter");
+		SparkConf conf = new SparkConf().setAppName("ListCounter");
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		List<Integer> data = Arrays.asList(1,23,45,21,26,90,19,0);
 		JavaRDD<Integer> distData = sc.parallelize(data);
